b361cf0496c03ba7d7075e13d3c71d9c9e353687
igor-hh <igor.budarov@accenture.com>
2019-08-16 15:54:02 +0300

    increase flower stock via SOAP client in loop

--- src/main/java/com/accenture/flowershop/be/util/SOAPClient.java
+++ src/main/java/com/accenture/flowershop/be/util/SOAPClient.java
@@ -5,6 +5,7 @@ import com.accenture.flowershop.be.business.service.FlowerStockWebService;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.net.URL;
+import java.util.Random;
 
 public class SOAPClient {
     public static void main(String[] args) {
@@ -13,7 +14,17 @@ public class SOAPClient {
             QName qname = new QName("http://impl.service.business.be.flowershop.accenture.com/", "FlowerStockWebServiceImplService");
             Service service = Service.create(url, qname);
             FlowerStockWebService flowerStockWebService = service.getPort(FlowerStockWebService.class);
-            System.out.println(flowerStockWebService.increaseFlowerStock(5) ? "Success" : "Something went wrong");
+
+            //infinite loop
+            while(true) {
+                try {
+                    int quantity = new Random().nextInt(21) + 10; //random int [10-30]
+                    System.out.println(flowerStockWebService.increaseFlowerStock(quantity) ? "Success, quantity: " + quantity : "Something went wrong");
+                    Thread.sleep(600000); //1000 * 60 * 10 = 10 minutes
+                } catch (InterruptedException i) {
+                    i.printStackTrace();
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
